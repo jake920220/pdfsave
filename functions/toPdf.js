8f0152698f1b12854136b7f357ef3016139f61fd
김준현 <jake220@naver.com>
2018-12-30 18:06:10 +0900

    1: api세팅, 스타일시트세팅

---
+++ functions/toPdf.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const puppeteer = require('puppeteer');
+const handlebars = require('handlebars');
+const readFile = util.promisify(fs.readFile);
+
+
+const obj = {
+  templateHtml: async(fileName, reqData) => {
+    try {
+      const data = reqData;
+
+      const templatePath = path.resolve('htmls', `${fileName}.html`);
+      const content = await readFile(templatePath, 'utf8');
+
+      const template = handlebars.compile(content);
+
+      return template(data);
+    } catch (error) {
+      throw new Error('Cannot create HTML template.');
+    }
+  },
+
+  createPdf: async(fileName, data) => {
+    const html = await this.htmlToPdf.templateHtml(fileName, data);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html);
+
+    await page.pdf({path: path.resolve('pdfs', `${fileName}.pdf`), format:'A4'});
+    return browser.close();
+  }
+};
+
+module.exports.htmlToPdf = obj;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const pdfModule = require('../functions/toPdf');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  console.log("qt");
+  pdfModule.htmlToPdf.test();
+  console.log(pdfModule.htmlToPdf);
+  // res.render('index', { title: 'Express' });
+});
+
+router.get('/test/:fileName', async (req, res, next) => {
+  const fileName = req.params.fileName;
+  await pdfModule.htmlToPdf.createPdf(fileName);
+  res.send("done");
 });
 
 module.exports = router;
